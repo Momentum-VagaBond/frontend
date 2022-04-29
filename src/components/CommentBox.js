7559519db1d76ae1b2af03df87789ec4f03a9280
Amy Gori <amydgori@gmail.com>
2022-04-29 14:14:17 -0400

    Add example for adding a new comment without a reload

--- src/components/CommentBox.js
+++ src/components/CommentBox.js
@@ -2,63 +2,84 @@ import { useState } from 'react'
 import axios from 'axios'
 import { useParams } from 'react-router-dom'
 
-import { Navigate, Link as RouterLink} from 'react-router-dom'
-import { AlertTile, TextField, Alert, Container, Typography, Box, Grid, Link, Checkbox, FormControlLabel, CssBaseline, Button, Avatar, CircularProgress } from '@mui/material';;
+import { Navigate, Link as RouterLink } from 'react-router-dom'
+import {
+  AlertTile,
+  TextField,
+  Alert,
+  Container,
+  Typography,
+  Box,
+  Grid,
+  Link,
+  Checkbox,
+  FormControlLabel,
+  CssBaseline,
+  Button,
+  Avatar,
+  CircularProgress,
+} from '@mui/material'
 
 // import { Link } from 'react-router-dom'
 
-export default function CommentBox({ token, logId }) {
-    const [commentPosted, setCommentPosted] = useState('')
-    const [comment, setComment] = useState("")
-    // const params = useParams()
+export default function CommentBox({ token, logId, setNewComment }) {
+  const [commentPosted, setCommentPosted] = useState('')
+  const [comment, setComment] = useState('')
+  // const params = useParams()
 
-    const handleComment = (e) => {
-      e.preventDefault()
-      axios
-        .post ("https://momentum-vagabond.herokuapp.com/api/log/9/comment/", {
-    
-          "log": 9,
-          "comments": comment
-      },
-      {
-        headers: {
-          'Authorization': `Token ${token}`
+  const handleComment = (e) => {
+    e.preventDefault()
+    axios
+      .post(
+        'https://momentum-vagabond.herokuapp.com/api/log/9/comment/',
+        {
+          log: 9,
+          comments: comment,
+        },
+        {
+          headers: {
+            Authorization: `Token ${token}`,
+          },
         }
-      }
-      ).then(res => {
-        console.log("post comment")
-        setComment('')
-        setCommentPosted("")
-          
+      )
+      .then((res) => {
+        console.log('post comment')
+        console.log(res.data)
+        // setComment('')
+        // setCommentPosted('')
+        setNewComment(res.data)
       })
   }
 
-    const handleChange = (inputType, e) => {
-        if (inputType === 'commentText') {
-          setComment(e.target.value)
-        }
+  const handleChange = (inputType, e) => {
+    if (inputType === 'commentText') {
+      setComment(e.target.value)
     }
+  }
 
-    return (
-      <>
-        <Box component='form' noValidate onSubmit={handleComment} sx={{ mt: 3 }}>    
-          <TextField
-            className='input-field'
-            // id="comment"
-            type='text'
-            required
-            fullWidth
-            placeholder="Add a comment..."
-            value={comment}
-            autoFocus
-            onChange={(e) => handleChange('commentText', e)}
-          />  
-          <Button type="submit"
-            fullWidth
-            variant="contained"
-            sx={{ mt: 3, mb: 2 }}>Post comment
-          </Button>
-        </Box>
-      </>
-    )
+  return (
+    <>
+      <Box component="form" noValidate onSubmit={handleComment} sx={{ mt: 3 }}>
+        <TextField
+          className="input-field"
+          // id="comment"
+          type="text"
+          required
+          fullWidth
+          placeholder="Add a comment..."
+          value={comment}
+          autoFocus
+          onChange={(e) => handleChange('commentText', e)}
+        />
+        <Button
+          type="submit"
+          fullWidth
+          variant="contained"
+          sx={{ mt: 3, mb: 2 }}
+        >
+          Post comment
+        </Button>
+      </Box>
+    </>
+  )
 }
