076ceaa4afa4133ad4753f92f6296928da76601e
Brittany Craig <brittanycraig@gmail.com>
2022-05-08 18:30:49 -0400

    removed background 5

--- src/routes/SubscriberHome.js
+++ src/routes/SubscriberHome.js
@@ -10,7 +10,7 @@ import { useParams } from 'react-router-dom'
 import { NEWTripDetailCard } from "../components/NEWTripDetailCard";
 import CardActionArea from '@mui/material/CardActionArea';
 import { StartTripCard } from "../components/StartTripCard";
-import Background2 from './Background2.png'
+// import Background2 from './Background2.png'
 
 
 
@@ -110,7 +110,7 @@ return (
             // flexDirection: 'column',
             // alignItems: 'center',
             // position: 'sticky',
-            backgroundImage: `url(${Background2})`,
+            // backgroundImage: `url(${Background2})`,
             backgroundSize: 'cover',
             height: "100%",
             paddingBottom: 15,
